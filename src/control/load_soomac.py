b1a8cec881cf1b519f6f1045748fcf5ca082a6f9
choiyoonji <cyj21c6352@gmail.com>
2024-07-09 23:57:53 +0900

    Add: doosan robot description, soomac description, isaacgym load

--- src/control/load_soomac.py
+++ src/control/load_soomac.py
@@ -12,8 +12,8 @@ sim_params.dt = 1 / 60
 sim_params.substeps = 2
 sim_params.use_gpu_pipeline = False
 
-sim_params.physx.num_position_iterations = 5
-sim_params.physx.num_velocity_iterations = 1
+sim_params.physx.num_position_iterations = 6
+sim_params.physx.num_velocity_iterations = 0
 sim_params.physx.rest_offset = 0.001
 sim_params.physx.contact_offset = 0.02
 sim_params.physx.use_gpu = True
@@ -31,27 +31,27 @@ plane_params.restitution = 0
 # create the ground plane
 gym.add_ground(sim, plane_params)
 # asset_root = '/home/choiyoonji/isaacgym/python/RL-study/ur5e/ur_description'
-asset_root = "/home/choiyj/catkin_ws/src/soomac/asset/Soomac_prototype_v1_description/urdf/"
-# asset_root = "/home/choiyj/catkin_ws/src/soomac/asset/dsr_description/urdf/"
-# asset_file = "m0609.urdf"
-asset_file = "soomac_v1.urdf"
+# asset_root = "/home/choiyj/catkin_ws/src/soomac/asset/soomac_description/urdf/"
+asset_root = "/home/choiyj/catkin_ws/src/soomac/asset/dsr_description/urdf/"
+asset_file = "m0609.urdf"
+# asset_file = "soomac.urdf"
 # asset_file = "lk_ur5e_with_allegro_right.urdf"
 asset_options = gymapi.AssetOptions()
-asset_options.fix_base_link = True
-asset_options.flip_visual_attachments = False
+# asset_options.fix_base_link = True
+# asset_options.flip_visual_attachments = False
 # asset_options.armature = 0.01
 asset = gym.load_asset(sim, asset_root, asset_file, asset_options)
-num_ur5e_bodies = gym.get_asset_rigid_body_count(asset)
-num_ur5e_dofs = gym.get_asset_dof_count(asset)
+num_robot_bodies = gym.get_asset_rigid_body_count(asset)
+num_robot_dofs = gym.get_asset_dof_count(asset)
 body_info = gym.get_asset_rigid_body_dict(asset)
 
-print('rigid body : ', num_ur5e_bodies)
-print('DOFs : ', num_ur5e_dofs)
+print('rigid body : ', num_robot_bodies)
+print('DOFs : ', num_robot_dofs)
 print('body dict : ', body_info)
 
-num_envs = 12
+num_envs = 1
 envs_per_row = 12
-env_spacing = 2.0
+env_spacing = 0.5
 env_lower = gymapi.Vec3(-env_spacing, 0.0, -env_spacing)
 env_upper = gymapi.Vec3(env_spacing, env_spacing, env_spacing)
 
@@ -67,9 +67,10 @@ for i in range(num_envs):
     height = 2.0
 
     pose = gymapi.Transform()
-    pose.p = gymapi.Vec3(0.0, height, 0.0)
+    pose.p = gymapi.Vec3(10.0, height, 0.0)
 
     actor_handle = gym.create_actor(env, asset, pose, "MyActor", i, 1)
+    print('ouhfowefhoiwejfweo')
     actor_handles.append(actor_handle)
     
 cam_props = gymapi.CameraProperties()
