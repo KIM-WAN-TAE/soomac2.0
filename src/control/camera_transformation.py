505355af2fc3c2757b1f22f11a06cfeeea6663a0
akxodms <150228780+akxodms@users.noreply.github.com>
2024-08-29 14:17:49 +0900

    define 함수

--- src/control/camera_transformation.py
+++ src/control/camera_transformation.py
@@ -1,47 +1,48 @@
 import numpy as np
 
-def rotate_x(vector, degree):
-    # Degree to Radian
-    rad = np.radians(degree)
+## 정의 자세 : x, y축 리버스(- or 180도 회전) / z축 
 
-    # Rotation Matrix
-    rotation_matrix = np.array([
-        [1, 0, 0],
-        [0, np.cos(rad), -np.sin(rad)],
-        [0, np.sin(rad), np.cos(rad)]
+def transformation_define(pose):
+    _pose_matrix = np.append(pose, 1)
+    pose_matrix = _pose_matrix.T
+
+    translation_matrix = np.array([
+        [1,0,0, 0],
+        [0,1,0,-35],
+        [0,0,1,370],
+        [0,0,0,1]
     ])
 
-    return np.dot(rotation_matrix, vector)
+    rotation_matrix_z = np.array([
+        [np.cos(np.radians(180)), -np.sin(np.radians(180)), 0, 0],
+        [np.sin(np.radians(180)), np.cos(np.radians(180)), 0, 0],
+        [0, 0, 1, 0],
+        [0,0,0,1]
+    ])
+    
+    rotation_matrix_x = np.array([
+        [1, 0, 0, 0],
+        [0, np.cos(np.radians(90+39)), -np.sin(np.radians(90+39)), 0],
+        [0, np.sin(np.radians(90+39)), np.cos(np.radians(90+39)), 0],
+        [0,0,0,1]
+    ])
 
-def translate(vector, translation):
-    return vector + translation 
+    T_g_to_camera = np.dot(np.dot(translation_matrix,rotation_matrix_z), rotation_matrix_x)
 
-
-
-def transformation_init(pose):
-
-    translate_vector = np.array([0, -0.1057, 0.301625]) # m 기준
-    rotate_degree = -34.5
-
-    vector = pose[:3]
-    rotated_vector = rotate_x(vector, rotate_degree)
-    translated_vector = translate(rotated_vector, translate_vector)
-
-    print(translated_vector)
-
-    return np.r_[translated_vector, pose[3]]
+    pose = np.dot(T_g_to_camera, pose_matrix)
+    return pose[:3] 
 
 
 # 수정 필요
-def transformation_cam(pose):
+def transformation_camera(pose):
 
     translate_vector = np.array([0, 0.2, 0.3])
     rotate_degree = -30
 
     vector = pose[:3]
-    rotated_vector = rotate_x(vector, rotate_degree)
-    translated_vector = translate(rotated_vector, translate_vector)
+    # rotated_vector = rotate_x(vector, rotate_degree)
+    # translated_vector = translate(rotated_vector, translate_vector)
 
-    print(translated_vector)
+    # print(translated_vector)
 
-    return np.r_[translated_vector, pose[3]]
+    # return np.r_[translated_vector, pose[3]]
