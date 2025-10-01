87436e2b8d64a2a355e22f5582cee39d5f7dc779
조영우 <joyeong-u@joyeong-us-MacBook-Pro.local>
2025-10-02 00:22:19 +0900

    free mission branch

---
+++ src/zeus_controller/zeus_controller/zeus_client copy.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from zeus_interfaces.srv import ZeusExecutor
+from zeus_interfaces.msg import ZeusMainCommand
+
+import threading, time
+import numpy as np
+
+RATE = 10
+
+def mparam_command_string(param_type, value):
+    if isinstance(value, (list, tuple)):
+        value_str = ",".join([str(v) for v in value])
+    else:
+        value_str = str(value)
+    cmd = f"mparam+{param_type},{value_str}"
+    return cmd
+
+class ZeusClientNode(Node):
+    def __init__(self):
+        super().__init__('zeus_client_node')
+        self.get_logger().info('[ZEUS] Client Node On!')
+
+        self.lock = threading.Lock()
+        
+        self.zeus_client = self.create_client(ZeusExecutor, 'zeus_executor')
+        while not self.zeus_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().warn('Service not available, waiting again...')
+            return
+        
+        self.binary_cmd = self.create_publisher(String, '/zeus/string/binary_command', 10)
+        
+        self.create_subscription(ZeusMainCommand, '/zeus/custom/client_command', self.target_pose_callback, 10)
+        
+        self.target_flag = False
+        self.target_frame = None
+        self.target_point = np.array([])
+        self.target_speed = None
+        
+        timer_period = 1/RATE
+        self.client_timer = self.create_timer(timer_period, self.client_timer_callback)
+    
+    def send_speed(self, frame, value):
+        cmd = mparam_command_string(frame, value)
+        msg = String()
+        msg.data = str(cmd)
+        self.binary_cmd.publish(msg)
+        
+    def send_speed_command_for_idx(self, idx):
+        with self.lock:
+            if idx in self.speed_cmd_idx_map and idx not in self.sent_speed_cmd_idx:
+                param_type, value = self.speed_cmd_idx_map[idx]
+                self.send_speed(param_type, value)
+                self.sent_speed_cmd_idx.add(idx)
+                # print(f'Speeeeeeeeeeeed : {idx}, {value}')
+    
+    def target_pose_callback(self, msg : ZeusMainCommand):
+        with self.lock:
+            self.target_frame = msg.frame
+            self.target_point = np.array(msg.position)
+            self.target_speed = msg.speed
+            self.target_flag = True
+            
+            self.get_logger().info(f'[AIOT] Target Point Received : {self.target_point}, Frame : {self.target_frame}, Speed : {self.target_speed}')
+    
+    def client_timer_callback(self):
+        self.get_logger().info(f'[AIOT] Flag State : {self.target_flag}')
+        
+        if not self.target_flag:
+            self.get_logger().info('[AIOT] Waiting For Target Point ...')
+            return
+
+        if self.target_flag and self.target_point.size != 0:
+            copy_target_point = self.target_point
+            copy_target_frame = self.target_frame
+            copy_target_speed = self.target_speed
+            self.target_flag = False
+            
+    def send_next_command(self, point, frame):
+        if point.size != 6:
+            self.get_logger().warn('[ZEUS] Invalid Target Point Size')
+            return
+        
+        req = ZeusExecutor.Request()
+        req.frame = frame
+        req.position = point
+        
+        future = self.zeus_client.call_async(req)
+        future.add_done_callback(self.response_callback)
+        
+    def response_callback(self, future):
+        try:
+            res = future.result()
+        except Exception as e:
+            self.get_logger().warn(f'[ZEUS] 서비스 호출 실패: {e}')
+            return
+        if res.success:
+            self.get_logger().info(f'[ZEUS] {self.idx+1}번째 명령 성공')
+        else:
+            self.get_logger().warn(f'[ZEUS] {self.idx+1}번째 명령 실패')
+        self.target_flag = False
+            
+        
+def main(args=None):
+    rclpy.init(args=args)
+    node = ZeusClientNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        print("\n\nShutting down ZEUS Service Client...")
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
