b2f5fd0b30ec3d09483f2e5c3ac5cd47a0738d59
Sagum0 <jyw010704@naver.com>
2025-10-29 17:39:46 +0900

    gripper update

--- src/zeus_controller/zeus_controller/zeus_block_gripper.py
+++ src/zeus_controller/zeus_controller/zeus_block_gripper.py
@@ -24,7 +24,7 @@ class GripperControllerNode(Node):
 
         try:
             # 1단계에서 확인한 아두이노 포트 이름을 여기에 적어주세요.
-            self.ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+            self.ser = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
             time.sleep(2) 
             self.get_logger().info('Serial port /dev/ttyACM0 opened successfully.')
         except serial.SerialException as e:
